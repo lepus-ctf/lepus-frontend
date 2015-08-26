e916a5fed3b6021885628159abcc3b3e027a6966
Yuki MIZUNO <mizuyuu0904@gmail.com>
2015-08-26 20:23:41 +0900

    Change build command

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var packager = require('electron-packager');
 
 gulp.task('compile', function(){
 	return gulp.src('src/**/*.{js,jsx}')
@@ -10,3 +11,75 @@ gulp.task('compile', function(){
 		)
 		.pipe(gulp.dest('js'));
 });
+
+var commonOption = {
+		dir: './',
+		out: 'release',
+		name: 'TDUCTF-2015',
+		arch: 'all',
+		platform: 'all',
+		ignore: [
+			'./node_modules/gulp*',
+			'./node_modules/electron*',
+			'./node_modules/semantic-ui',
+			'./node_modules/yamljs',
+			'./node_modules/.bin',
+			'./release',
+			'./src',
+			'./.git*'
+		],
+		version: '0.30.5'
+}
+
+gulp.task('package:darwin', ['compile'], function (done) {
+	var option = commonOption;
+	option.platform = 'darwin';
+	option.arch = 'x64';
+	packager(option, function (err, path) {
+		done();
+	});
+});
+
+gulp.task('package:linux:ia32', ['compile'], function (done) {
+	var option = commonOption;
+	option.platform = 'linux';
+	option.arch = 'ia32';
+	packager(option, function (err, path) {
+		done();
+	});
+});
+
+gulp.task('package:linux:x64', ['compile'], function (done) {
+	var option = commonOption;
+	option.platform = 'linux';
+	option.arch = 'x64';
+	packager(option, function (err, path) {
+		done();
+	});
+});
+
+gulp.task('package:win:ia32', ['compile'], function (done) {
+	var option = commonOption;
+	option.platform = 'win';
+	option.arch = 'ia32';
+	packager(option, function (err, path) {
+		done();
+	});
+});
+
+gulp.task('package:win:x64', ['compile'], function (done) {
+	var option = commonOption;
+	option.platform = 'win';
+	option.arch = 'x64';
+	packager(option, function (err, path) {
+		done();
+	});
+});
+
+gulp.task('package:all', ['compile'], function (done) {
+	var option = commonOption;
+	packager(option, function (err, path) {
+		done();
+	});
+});
+
