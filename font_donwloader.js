9b8a554b332613d4487fde119d887e3aff9df9a7
Yuki MIZUNO <mizuyuu0904@gmail.com>
2015-09-01 14:12:47 +0900

    Improve font downloader

--- font_donwloader.js
+++ font_donwloader.js
@@ -1,14 +1,16 @@
-//var url = "https://github.com/adobe-fonts/source-han-sans/archive/1.004R.tar.gz";
 var urls = [
 "http://mplus-fonts.osdn.jp/webfonts/mplus-2m-regular.ttf",
 "http://mplus-fonts.osdn.jp/webfonts/mplus-2p-regular.ttf",
 ];
 
-//var url = "http://localhost:8000/source-han-code-jp-1.004R.tar.gz";
 var fs = require('fs');
 var path = require('path');
 var dirname = "font"
 
+if (fs.existsSync(dirname)) {
+	// Already exists. Skip downloading font.
+	return true;
+}
 fs.mkdirSync(dirname);
 
 urls.forEach(function(url) {
